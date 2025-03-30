53bba039331fecea39dd8c71fb3ac7c88defdbd0
Meghna16-27 <meghnapaul2601@gmail.com>
2025-03-30 17:25:14 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV meghna')
+  res.send('Hello MCKVians')
 })
 
 app.get('/courses', function (req, res) {
